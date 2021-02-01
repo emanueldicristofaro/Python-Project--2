d4dd5d0f5c9d1bbfee7e76b8e61ee62f2d88f9f9
Boris Torrado <boristor22@gmail.com>
2021-02-01 00:42:06 -0400

    Bebidas al catalogo

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
-from .models import Tamano, Ingrediente
+from .models import Tamano, Ingrediente, Bebida
+
+pedido = list()
 
 def index(request):
+
+    if request.method == 'POST':
+
+        tamano = request.POST.get("size")
+        lista_ingredientes = request.POST.getlist('ingredients')
+        lista_bebidas = request.POST.getlist('drinks')
+
+        if request.POST.get("actionbtn") == "end":
+            # render para detalles del pedido
+            pedido.clear()
+            print("post")
+        else:
+            sandwich = {
+                "size": tamano,
+                "ingredients": lista_ingredientes,
+                "drinks": lista_bebidas
+            }
+            pedido.append(sandwich)
+            print(pedido)
+
     tamanos = Tamano.objects.all()
     ingredientes = Ingrediente.objects.all()
-    contex = {'tamanos' : tamanos, 'ingredientes' : ingredientes}
+    bebidas = Bebida.objects.all()
+    contex = {'tamanos': tamanos, 'ingredientes': ingredientes, 'bebidas' : bebidas}
     return render(request, 'pedidos/index.html', contex)
