77fada8cf2d7aa5cb8430a893c4abd70d8cd0111
Emanuel Di Cristofaro <emanuelesposito3@gmail.com>
2021-02-02 01:51:29 -0400

    INSERTS - Actualización 🐍
    -Inserts realizados
    -Se acomodaron los templates de reportes.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,100 @@
 from django.shortcuts import render
-from .models import Tamano, Ingrediente, Bebida, Sandwich, Sandwich_Ingrediente, Bebida_Sandwich
+from django.utils import timezone 
+from .models import Pedido, Tamano, Ingrediente, Bebida, Sandwich, Sandwich_Ingrediente, Bebida_Sandwich
 
 
 # Pagina principal para que se traiga toda la data y las opciones de pedido
 #######################################################################
 
 pedido = list()
+
+def insertPedido(pedido):
+
+    # Obtener fecha actual
+    fechaActual = timezone.now()
+
+    size = []
+    ingredients = []
+    drinks = []
+
+    '''pedidoInsert = Pedido.objects.create(fechaPedido = fechaActual, nombreCliente = "Emanuel")'''
+
+    #Operaciones de insert
+    for pe in pedido:
+
+        for key, value in pe.items():
+
+           if key == "size":
+               
+              size.append(value)
+
+           elif key == "ingredients":
+
+              ingredients.append(value)
+
+           elif key == "drinks":
+
+              drinks.append(value)
+
+    #Insert in sandwich 
+    idCliente = Pedido.objects.get(nombreCliente = "Emanuel")
+    nombreClienteString = idCliente.nombreCliente    
+        
+    for s in size:
+        sizeId = Tamano.objects.get(id_tamano = s)
+        sandwich = Sandwich(fk_pedido = idCliente, fk_tamano = sizeId)
+        sandwich.save()
+
+    #Insert bebidas in sandwich
+
+    QuerySandwichs = '''SELECT polls_sandwich.id_sandwich FROM polls_sandwich 
+    WHERE polls_sandwich.fk_pedido_id = (SELECT polls_pedido.id_pedido FROM polls_pedido WHERE polls_pedido.nombreCliente = %s)'''
+
+    listaSandwiches = Sandwich.objects.raw(QuerySandwichs, [nombreClienteString])
+    listaSandwichesNueva = list()
+
+    for sand in listaSandwiches:
+        listaSandwichesNueva.append(sand.id_sandwich)
+
+    contador = 0
+    #Procedure 
+    for b in drinks:
+
+        for bed in b:
+
+            drinkId = Bebida.objects.get(id_bebida = bed)
+            sandwichId = Sandwich.objects.get(id_sandwich = listaSandwichesNueva[contador])
+            sandwich_bebida = Bebida_Sandwich(fk_sandwich = sandwichId, fk_bebida = drinkId)
+            sandwich_bebida.save()
+            
+        contador = contador + 1
+
+    #Insert ingredientes in sandwich
+
+    contador_2 = 0
+    #Procedure
+    for i in ingredients:
+
+        for ing in i:
+
+            ingredientId = Ingrediente.objects.get(id_ingrediente = ing)
+            sandwichId = Sandwich.objects.get(id_sandwich = listaSandwichesNueva[contador_2])
+            ingredientes_sandwiches = Sandwich_Ingrediente(fk_sandwich = sandwichId, fk_ingrediente = ingredientId)
+            ingredientes_sandwiches.save()
+
+        contador_2 = contador_2 + 1
+
+    #########################################
+    #Resetear las listas
+    size.clear()
+    ingredients.clear()
+    drinks.clear()
+    contador = 0
+    contador_2 = 0
+    listaSandwichesNueva.clear()
+    #########################################
+      
+    return idCliente
 
 def index(request):
 
@@ -16,11 +105,22 @@ def index(request):
         lista_bebidas = request.POST.getlist('drinks')
 
         if request.POST.get("actionbtn") == "end":
+            
+            #Realizar el insert
+
+            context = insertPedido(pedido)
+
             # render para detalles del pedido
             pedido.clear()
             print("post")
+
+            # Redirigir a la pagina de fatura
+        
+            return render(request, 'pedidos/factura.html')
+
         else:
             sandwich = {
+
                 "size": tamano,
                 "ingredients": lista_ingredientes,
                 "drinks": lista_bebidas
@@ -39,7 +139,7 @@ def index(request):
 # Factura
 #######################################################################
 
-def factura(request):
+def factura(request, pedido):
 
     return render(request, 'pedidos/factura.html')
 
